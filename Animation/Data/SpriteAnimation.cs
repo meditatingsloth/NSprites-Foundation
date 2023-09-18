ab122431f837ea8af51c4b96f5f3fc093b82fb56
Austin Borden <austin.borden@gmail.com>
2023-09-18 08:15:26 +0700

    allow limiting frame count in animation

--- Animation/Data/SpriteAnimation.cs
+++ Animation/Data/SpriteAnimation.cs
@@ -1,32 +1,14 @@
 ﻿using Unity.Mathematics;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewNSpriteAnimation", menuName = "NSprites/Animation (frame sequence)")]
+[CreateAssetMenu(
+    fileName = "NewNSpriteAnimation",
+    menuName = "NSprites/Animation (frame sequence)"
+)]
 public class SpriteAnimation : ScriptableObject
 {
     // Sprite here required because whe want to know UV of animation frame sequence on atlas
     public Sprite SpriteSheet;
     public int2 FrameCount = new(1);
     public float[] FrameDurations = new float[1] { 0.1f };
-
-    #region Editor
-#if UNITY_EDITOR
-    private const float DefaultFrameDuration = .1f;
-    private void OnValidate()
-    {
-        var frameCount = FrameCount.x * FrameCount.y;
-        if (FrameDurations.Length != frameCount)
-        {
-            var correctedFrameDurations = new float[frameCount];
-            var minLength = math.min(FrameDurations.Length, correctedFrameDurations.Length);
-            for (int i = 0; i < minLength; i++)
-                correctedFrameDurations[i] = FrameDurations[i];
-            for (int i = minLength; i < correctedFrameDurations.Length; i++)
-                correctedFrameDurations[i] = DefaultFrameDuration;
-            FrameDurations = correctedFrameDurations;
-        }
-    }
-#endif
-
-    #endregion
 }
--- Animation/Systems/SpriteUVAnimationSystem.cs
+++ Animation/Systems/SpriteUVAnimationSystem.cs
@@ -4,8 +4,8 @@ using Unity.Burst;
 
 namespace NSprites
 {
-    // TODO: check animation system can work with different frame size animations 
-    
+    // TODO: check animation system can work with different frame size animations
+
     /// Compare <see cref="AnimationTimer"/> with global time and switch <see cref="FrameIndex"/> when timer expired.
     /// Perform only not-culled entities. Restore <see cref="FrameIndex"/> and duration time for entities which be culled for some time.
     /// Somehow calculations goes a bit wrong and unculled entities gets synchronized, don't know how to fix
@@ -17,18 +17,20 @@ namespace NSprites
         {
             public double Time;
 
-            private void Execute(ref AnimationTimer animationTimer,
-                                    ref FrameIndex frameIndex,
-                                    ref UVAtlas uvAtlas,
-                                    in AnimationSetLink animationSet,
-                                    in AnimationIndex animationIndex)
+            private void Execute(
+                ref AnimationTimer animationTimer,
+                ref FrameIndex frameIndex,
+                ref UVAtlas uvAtlas,
+                in AnimationSetLink animationSet,
+                in AnimationIndex animationIndex
+            )
             {
                 var timerDelta = Time - animationTimer.value;
 
                 if (timerDelta >= 0f)
                 {
                     ref var animData = ref animationSet.value.Value[animationIndex.value];
-                    var frameCount = animData.GridSize.x * animData.GridSize.y;
+                    var frameCount = animData.FrameDurations.Length;
                     frameIndex.value = (frameIndex.value + 1) % frameCount;
                     var nextFrameDuration = animData.FrameDurations[frameIndex.value];
 
@@ -47,12 +49,21 @@ namespace NSprites
                     animationTimer.value = Time + nextFrameDuration;
 
                     var frameSize = new float2(animData.UVAtlas.xy / animData.GridSize);
-                    var framePosition = new int2(frameIndex.value % animData.GridSize.x, frameIndex.value / animData.GridSize.x);
-                    uvAtlas = new UVAtlas { value = new float4(frameSize, animData.UVAtlas.zw + frameSize * framePosition) };
+                    var framePosition = new int2(
+                        frameIndex.value % animData.GridSize.x,
+                        frameIndex.value / animData.GridSize.x
+                    );
+                    uvAtlas = new UVAtlas
+                    {
+                        value = new float4(
+                            frameSize,
+                            animData.UVAtlas.zw + frameSize * framePosition
+                        )
+                    };
                 }
             }
         }
-        
+
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
